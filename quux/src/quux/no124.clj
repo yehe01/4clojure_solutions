928215d5f8be04345b1bb066411a9d0f3d214ae7
luckykevin <htt5156@gmail.com>
2013-05-18 10:31:45 +0800

    Refactored to remove duplication.

--- quux/src/quux/no124.clj
+++ quux/src/quux/no124.clj
@@ -13,55 +13,8 @@
                    j (range (count (first b)))]
                (get-in b [j i]))))
 
-(defn legal? [board s e]
-  (let [color (get-in board s)
-        check-row (fn [b r c1 c2]
-                    (let [small (min c1 c2)
-                          big (max c1 c2)
-                          line (drop (inc small) (take big (nth b r)))]
-                      (and (not (empty? line)) (every? #(and (not= % color) (not= % 'e)) line))))
-        check-diagnal (fn [x1 y1 xr yr]
-                        (let [r (map (fn [[xi yi]] (get-in board (vector (+ x1 xi) (+ y1 yi)))) (partition 2 (interleave xr yr)))]
-                          (and (not (empty? r)) (every? #(and (not= % color) (not= % 'e)) r))))
-        x1 (first s)
-        x2 (first e)
-        y1 (second s)
-        y2 (second e)
-        xr (if (> (- x2 x1) 0) (drop 1 (range (- x2 x1))) (sort > (drop 1 (range (- x2 x1) 0))))
-        yr (if (> (- y2 y1) 0) (drop 1 (range (- y2 y1))) (sort > (drop 1 (range (- y2 y1) 0))))]
-    (cond (= x1 x2) (check-row board x1 y1 y2)
-          (= y1 y2) (check-row (transpose board) y1 x1 x2)
-          (or (= -1 (/ (- x1 x2) (- y1 y2))) (= 1 (/ (- x1 x2) (- y1 y2)))) (check-diagnal x1 y1 xr yr)
-          :else false)))
-
 (defn legal-moves [board start]
-  (set (filter #(legal? board start %) (candidates board 'e))))
-
-(defn flipped-moves [board start]
-  (let [moves (legal-moves board start)
-        flip-move (fn [s e]
-                    (let [flip-row (fn [r c1 c2]
-                                     (let [small (min c1 c2)
-                                           big (max c1 c2)]
-                                       (set (map #(vector r %) (range (inc small) big)))))
-                          flip-colum (fn [c r1 r2]
-                                       (let [small (min r1 r2)
-                                             big (max r1 r2)]
-                                         (set (map #(vector % c) (range (inc small) big)))))
-                          flip-diagnal (fn [x1 y1 xr yr]
-                                         (set (map (fn [[xi yi]] (vector (+ x1 xi) (+ y1 yi))) (partition 2 (interleave xr yr)))))
-                          x1 (first s)
-                          x2 (first e)
-                          y1 (second s)
-                          y2 (second e)
-                          xr (if (> (- x2 x1) 0) (drop 1 (range (- x2 x1))) (sort > (drop 1 (range (- x2 x1) 0))))
-                          yr (if (> (- y2 y1) 0) (drop 1 (range (- y2 y1))) (sort > (drop 1 (range (- y2 y1) 0))))]
-                      (cond (= x1 x2) (flip-row x1 y1 y2)
-                            (= y1 y2) (flip-colum y1 x1 x2)
-                            :else (flip-diagnal x1 y1 xr yr))))]
-    (reduce #(assoc % %2 (flip-move start %2)) {} moves)))
-
-
+  (into {}  (filter (fn [[k v]] (not (empty? v))) (map #(vector % (legal-path board start %)) (candidates board 'e)))))
 
 (def bla
   (fn [board mark]
@@ -125,3 +78,34 @@
       (into {} (mapcat (fn [start]
                          (reduce #(assoc % %2 (flip-move start %2)) {} (legal-moves board start)))
                        (candidates board mark))))))
+
+(defn legal-path [board s e]
+  (let [color (get-in board s)
+        check-row (fn [b r c1 c2]
+                    (let [small (min c1 c2)
+                          big (max c1 c2)
+                          line (drop (inc small) (take big (nth b r)))]
+                      (and (not (empty? line)) (every? #(and (not= % color) (not= % 'e)) line))))
+        check-diagnal (fn [x1 y1 xr yr]
+                        (let [r (map (fn [[xi yi]] (get-in board (vector (+ x1 xi) (+ y1 yi)))) (partition 2 (interleave xr yr)))]
+                          (and (not (empty? r)) (every? #(and (not= % color) (not= % 'e)) r))))
+        x1 (first s)
+        x2 (first e)
+        y1 (second s)
+        y2 (second e)
+        xr (if (> (- x2 x1) 0) (drop 1 (range (- x2 x1))) (sort > (drop 1 (range (- x2 x1) 0))))
+        yr (if (> (- y2 y1) 0) (drop 1 (range (- y2 y1))) (sort > (drop 1 (range (- y2 y1) 0))))
+        flip-row (fn [r c1 c2]
+                   (let [small (min c1 c2)
+                         big (max c1 c2)]
+                     (set (map #(vector r %) (range (inc small) big)))))
+        flip-colum (fn [c r1 r2]
+                     (let [small (min r1 r2)
+                           big (max r1 r2)]
+                       (set (map #(vector % c) (range (inc small) big)))))
+        flip-diagnal (fn [x1 y1 xr yr]
+                       (set (map (fn [[xi yi]] (vector (+ x1 xi) (+ y1 yi))) (partition 2 (interleave xr yr)))))]
+    (cond (and (= x1 x2) (check-row board x1 y1 y2)) (flip-row x1 y1 y2)
+          (and (= y1 y2) (check-row (transpose board) y1 x1 x2)) (flip-colum y1 x1 x2)
+          (and (not= x1 x2) (not= y1 y2) (or (= -1 (/ (- x1 x2) (- y1 y2))) (= 1 (/ (- x1 x2) (- y1 y2)))) (check-diagnal x1 y1 xr yr)) (flip-diagnal x1 y1 xr yr)
+          :else #{})))
--- quux/test/quux/t_no124.clj
+++ quux/test/quux/t_no124.clj
@@ -11,71 +11,37 @@
                 [b b w b]
                 [e b b b]] 'e) => #{[0 0] [0 2] [3 0]})
 
-(fact "return if e is a legal move"
-  (legal? '[[e e e e]
-            [e w b e]
-            [e b w e]
-            [e e e e]] [1 1] [1 3]) => truthy
-  (legal? '[[e e e e]
-            [e w b e]
-            [e b w e]
-            [e e e e]] [1 1] [1 2]) => falsey
-  (legal? '[[e e w e]
-            [b b w e]
-            [b w w e]
-            [b w w w]] [3 0] [0 3]) => truthy
-  (legal? '[[e e e e]
-            [e w b e]
-            [e w w e]
-            [e e e e]] [1 1] [3 1]) => falsey
-  (legal? '[[e e e e]
-            [e w b e]
-            [e b w e]
-            [e e e e]] [2 2] [0 2]) => truthy
-  (legal? '[[e e e e]
-            [e w b e]
-            [w w w e]
-            [e e e e]] [1 2] [3 0]) => truthy
-  (legal? '[[e e e e]
-            [e w b e]
-            [e b w e]
-            [e e e e]] [1 1] [3 3]) => falsey
-  (legal? '[[e e e e]
-            [e w b e]
-            [w w w e]
-            [e e e e]] [1 2] [3 0]) => truthy)
-
 (fact "return legal moves from one candidate"
+  (legal-path '[[e e e e]
+                [e w b e]
+                [e b w e]
+                [e e e e]] [1 1] [1 3]) => #{[1 2]}
+  (legal-path '[[e e e e]
+                [e w b e]
+                [e b w e]
+                [e e e e]] [2 2] [0 2]) => #{[1 2]}
+  (legal-path '[[e e e e]
+                [e w b e]
+                [w w w e]
+                [e e e e]] [1 2] [3 0]) => #{[2 1]}
+  (legal-path '[[e e w e]
+                [b b w e]
+                [b w w e]
+                [b w w w]] [3 0] [0 3]) => #{[1 2] [2 1]})
+
+(fact "returns a map of start. its value a set of the coordinates of the pieces flipped by that move."
   (legal-moves '[[e e e e]
                  [e w b e]
                  [e b w e]
-                 [e e e e]] [1 1]) => #{[1 3] [3 1]}
+                 [e e e e]] [1 1]) => {[1 3] #{[1 2]}, [3 1] #{[2 1]}}
   (legal-moves '[[e e e e]
                  [e w b e]
                  [e b w e]
-                 [e e e e]] [2 2]) => #{[0 2] [2 0]}
+                 [e e e e]] [2 2]) => {[0 2] #{[1 2]}, [2 0] #{[2 1]}}
   (legal-moves '[[e e e e]
                  [e w b e]
                  [w w w e]
-                 [e e e e]] [1 2]) => #{[3 2] [3 0] [1 0]}
-  (legal-moves '[[e e w e]
-                 [b b w e]
-                 [b w w e]
-                 [b w w w]] [3 0]) => #{[0 3]})
-
-(fact "returns a map of start. its value a set of the coordinates of the pieces flipped by that move."
-  (flipped-moves '[[e e e e]
-                   [e w b e]
-                   [e b w e]
-                   [e e e e]] [1 1]) => {[1 3] #{[1 2]}, [3 1] #{[2 1]}}
-  (flipped-moves '[[e e e e]
-                   [e w b e]
-                   [e b w e]
-                   [e e e e]] [2 2]) => {[0 2] #{[1 2]}, [2 0] #{[2 1]}}
-  (flipped-moves '[[e e e e]
-                   [e w b e]
-                   [w w w e]
-                   [e e e e]] [1 2]) => {[3 0] #{[2 1]}, [1 0] #{[1 1]}, [3 2] #{[2 2]}})
+                 [e e e e]] [1 2]) => {[3 0] #{[2 1]}, [1 0] #{[1 1]}, [3 2] #{[2 2]}})
 
 (fact "bla"
   (bla '[[e e w e]
